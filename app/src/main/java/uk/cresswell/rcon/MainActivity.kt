96d7f3c6a857347172576ed2554eb5216869a3b1
David Cresswell <2761993+DavidCresswell@users.noreply.github.com>
2024-01-27 15:24:41 +0000

    Save server list to storage

--- app/src/main/java/uk/cresswell/rcon/MainActivity.kt
+++ app/src/main/java/uk/cresswell/rcon/MainActivity.kt
@@ -1,15 +1,18 @@
 package uk.cresswell.rcon
+
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var serverList: ArrayList<Server>
     private lateinit var serverAdapter: ArrayAdapter<Server>
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +26,12 @@ class MainActivity : AppCompatActivity() {
         serverList = ArrayList()
         serverAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, serverList)
         serverListView.adapter = serverAdapter
+
+        // Initialize SharedPreferences
+        sharedPreferences = getSharedPreferences("ServerList", MODE_PRIVATE)
+
+        // Load server list from SharedPreferences
+        loadServerList()
 
         addButton.setOnClickListener {
             // Open AddServerActivity to add a new server
@@ -40,8 +49,25 @@ class MainActivity : AppCompatActivity() {
             if (newServer != null) {
                 serverList.add(newServer)
                 serverAdapter.notifyDataSetChanged()
+
+                // Save server list to SharedPreferences
+                saveServerList()
             }
         }
+    }
+
+    private fun loadServerList() {
+        val serverListJson = sharedPreferences.getString("serverList", null)
+        if (serverListJson != null) {
+            val servers = Gson().fromJson(serverListJson, Array<Server>::class.java)
+            serverList.addAll(servers)
+            serverAdapter.notifyDataSetChanged()
+        }
+    }
+
+    private fun saveServerList() {
+        val serverListJson = Gson().toJson(serverList)
+        sharedPreferences.edit().putString("serverList", serverListJson).apply()
     }
 
     companion object {
